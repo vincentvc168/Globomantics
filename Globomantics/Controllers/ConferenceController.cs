d7b4089d2a704dcc5b36f9ab9e844377cb180bbb
vincent.hk.cn <vincent.hk.cn@outlook.com>
2020-06-09 23:04:45 +0800

    Add project files.

---
+++ Globomantics/Controllers/ConferenceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Globomantics.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Globomantics.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        public ConferenceController(IConferenceService conferenceService)
+        {
+
+        }
+    }
+}
